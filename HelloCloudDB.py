7cde972a8a3d59ab23fdbbfdffc30d26d2fc0f45
dreamthinnaphat <6106021612203@fitm.kmutnb.ac.th>
2021-01-26 09:25:42 +0700

    Create Hello Cloud

---
+++ HelloCloudDB.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+#Init app
+app = Flask(__name__)
+
+#Database
+app.config['SQLALCHEMY_DATABAES_URI'] = 'postgresql://webadmin:TIHnin12529@node8584-advweb-14.app.ruk-com.cloud:11094/CloudDB'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#Init db
+db = SQLAlchemy(app)
+#Init ma
+ma = Marshmallow(app)
+
+#Staff Class/Model
+class Staffs(db.Model):
+    id = db.Column(db.String(13), primary_key=True, unique=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(25))
+    phone = db.Column(db.String(10))
+    
+    def __init__(self, id, name, email, phone):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.phone = phone
+
+# Staff Schema
+class StaffSchema(ma.Schema):
+    class Meta:
+        fields =('id', 'name', 'email', 'phone')
+
+# Init Schema 
+staff_schema = StaffSchema()
+staffs_schema = StaffSchema(many=True)
+
+# Get All Staffs
+@app.route('/staffs', methods=['GET'])
+def get_staffs():
+    all_staffs = Staffs.query.all()
+    result = staffs_schema.dump(all_staffs)
+    return jsonify(result)
+
+# Web Root Hello
+@app.route('/', methods=['GET'])
+def get():
+    return jsonify({'ms': 'Hello Cloud DB1'})
+
+# Run Server
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=80)
